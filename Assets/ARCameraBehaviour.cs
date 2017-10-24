6241c498ca5553a8475ef4b3e8157c69162e8fc0
WitsBitz <roldan@thecosmicwhale.com>
2017-10-24 09:01:15 -0600

    initialize project with image target, vuforia integration, a few horror assets

---
+++ Assets/ARCameraBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class ARCameraBehaviour : MonoBehaviour {
+
+	[SerializeField] private Transform imageTarget;
+	[SerializeField] private Transform hellClown;
+	private HellportalTrackableEventHandler hellPortal;
+
+	void Awake()
+	{
+		hellClown.gameObject.SetActive(false);
+		hellPortal = imageTarget.GetComponent<HellportalTrackableEventHandler>();
+	}
+
+	void Update () 
+	{
+		if(imageTarget != null && hellPortal.IsActive)
+		{
+			float dist = Vector3.Distance(transform.position, imageTarget.position);
+			if(dist < .5f)
+				{
+					hellClown.gameObject.SetActive(true);
+				}
+		}
+	}
+}
---
+++ Assets/RotateHellCam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHellCam : MonoBehaviour {
+
+	private Vector3 rotationOffset = new Vector3( -90, -90, 90);
+
+
+	void Update () {
+		transform.rotation = Camera.main.transform.rotation * Quaternion.Euler(rotationOffset);
+	}
+}
